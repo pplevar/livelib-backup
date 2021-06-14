6a83fe3d09abe9ee2aaa94660d29180e9437432f
vazhukov <vazhukov_1@edu.hse.ru>
2021-06-14 13:26:06 +0300

    added check for the correctness of the username input

--- export.py
+++ export.py
@@ -2,6 +2,8 @@ from Helpers.livelib_parser import get_books, get_quotes, slash_add
 from Helpers.csv_reader import read_books_from_csv, read_quotes_from_csv
 from Helpers.csv_writer import save_books, save_quotes
 from Helpers.arguments import get_arguments
+from urllib import request
+import sys
 
 
 def get_new_items(old_data, new_data):
@@ -18,6 +20,13 @@ if __name__ == "__main__":
     ll_href = 'https://www.livelib.ru/reader'
 
     user_href = slash_add(ll_href, args.user)
+    try:
+        request.urlopen(user_href)
+    except Exception as ex:
+        print('\nERROR: Some troubles with downloading %s:' % user_href, ex)
+        print('Double-check your username')
+        sys.exit(1)
+
     book_file = args.books_backup or 'backup_%s_book.csv' % args.user
     quote_file = args.quotes_backup or 'backup_%s_quote.csv' % args.user
     print('Data from the page %s will be saved to files %s and %s' % (user_href, book_file, quote_file))
