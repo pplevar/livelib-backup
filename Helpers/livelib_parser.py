f619008fc38e779755941dfe5b5236db462fa946
vazhukov <vazhukov_1@edu.hse.ru>
2021-06-14 15:44:54 +0300

    add comments

--- Helpers/livelib_parser.py
+++ Helpers/livelib_parser.py
@@ -146,17 +146,21 @@ def book_parser(book_html, date, status):
     book_data = handle_xpath(book_html, './/div/div/div[@class="brow-data"]/div')
     if book_data is None:
         return error_handler('book_data', book_html)
+
     book_name = handle_xpath(book_data, './/a[contains(@class, "brow-book-name")]')
-    link = try_get_book_link(book_name.get("href"))
+    link = try_get_book_link(book_name.get("href"))  # в аргументах лежит ссылка
     if link is None:
         return error_handler('link', book_html)
     name = None if book_name is None else book_name.text
+
     author = book_data.xpath('.//a[contains(@class, "brow-book-author")]/text()')
     if len(author):
-        author = ', '.join(author)
+        author = ', '.join(author)  # в случае нескольких авторов нужно добавить запятые
+
     rating = None
     if status == 'read':
         rating = handle_xpath(book_data, './/div[@class="brow-ratings"]/span/span/span/text()')
+
     return Book(link, status, name, author, rating, date)
 
 
@@ -181,6 +185,8 @@ def quote_parser(quote_html):
     card = handle_xpath(quote_html, './/div[@class="lenta-card"]')
     if card is None:
         return error_handler('card', quote_html)
+
+    # Просматриваем все ссылки пока не найдем те, что нам подойдут
     link = None
     link_book = None
     for href in card.xpath('.//a'):
@@ -188,12 +194,16 @@ def quote_parser(quote_html):
             link = try_get_quote_link(href.get('href'))
         if link_book is None:
             link_book = try_get_book_link(href.get('href'))
+
     text = get_quote_text(card)
+    # Если мы нашли "Читать дальше...", нужно дать об этом знать и обработать во внешней функции
     if len(card.xpath('.//a[@class="read-more__link"]')):
         text = '!!!NOT_FULL###'
+
     book_card = handle_xpath(card, './/div[@class="lenta-card-book__wrapper"]')
     book_name = handle_xpath(book_card, './/a[@class="lenta-card__book-title"]/text()')
     book_author = handle_xpath(book_card, './/p[@class="lenta-card__author-wrap"]/a/text()')
+
     if link is not None and link_book is not None and text is not None:
         return Quote(link, text, Book(link_book, name=book_name, author=book_author))
     if link is None or link_book is None:
@@ -220,16 +230,21 @@ def get_books(user_href, status, min_delay=30, max_delay=60):
     href = slash_add(user_href, status)
     page_idx = 1
     page = None
+
     while True:
         wait_for_delay(min_delay, max_delay)
+
+        # если происходит какая-то ошибка с подключением, переходим к следующей странице
         try:
             page = html.fromstring(download_page(href_i(href, page_idx)))
         except Exception:
             continue
         finally:
             page_idx += 1
+
         if is_last_page(page) or is_redirecting_page(page):
             break
+
         last_date = None
         for div_book_html in page.xpath('.//div[@id="booklist"]/div'):
             date = handle_xpath(div_book_html, './/h2/text()')
@@ -241,6 +256,7 @@ def get_books(user_href, status, min_delay=30, max_delay=60):
                 book = book_parser(div_book_html, last_date, status)
                 if book is not None:
                     books.append(book)
+
     return books
 
 
@@ -256,25 +272,31 @@ def get_quotes(user_href, min_delay=30, max_delay=60):
     href = slash_add(user_href, 'quotes')
     page_idx = 1
     page = None
+
     while True:
         wait_for_delay(min_delay, max_delay)
+
+        # если происходит какая-то ошибка с подключением, переходим к следующей странице
         try:
             page = html.fromstring(download_page(href_i(href, page_idx)))
         except Exception:
             continue
         finally:
             page_idx += 1
+
         if is_last_page(page) or is_redirecting_page(page):
             break
+
         for quote_html in page.xpath('.//article'):
             quote = quote_parser(quote_html)
             if quote is not None and quote not in quotes:
-                if quote.text == '!!!NOT_FULL###':
+                if quote.text == '!!!NOT_FULL###':  # обрабатываем случай, когда показан не весь текст цитаты
                     wait_for_delay(min_delay, max_delay)
-                    try:
+                    try:  # просматриваем страницу цитаты, в случае ошибки переходим к следующей цитате
                         quote_page = html.fromstring(download_page(quote.link))
                     except Exception:
                         continue
                     quote.text = get_quote_text(handle_xpath(quote_page, './/article'))
                 quotes.append(quote)
+
     return quotes
